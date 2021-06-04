164ef659bfd8ede2a88d026a9c174a113e350221
Carla G. Chiodi <carla.chiodi@gmail.com>
2021-06-04 22:55:04 +0100

    Generate primes
    Created function to generate the prime array and to check if the number is prime

--- prime-tables.js
+++ prime-tables.js
@@ -1,11 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-const tableForm = document.querySelector("#generate-table")
-tableForm.addEventListener("submit", generateTable)
+  const tableForm = document.querySelector("#generate-table")
+  tableForm.addEventListener("submit", generateTable)
+  let primeArray = []
+
+  function isPrime(n) {
+    if (n <= 1) {
+      return false
+    } else if (n > 2) {
+      for(let i = 2; i < n; i++) {
+        if (n % i === 0) {
+          return false
+        } else {
+          return true
+        }
+      }
+    } else {
+      return true
+    }
+  }
+
+  function generatePrimeArray(n) {
+    let i = 0
+    let primeArray = []
+    while (primeArray.length < n) {
+      if (isPrime(i)) {
+        primeArray.push(i)
+      } i++
+    }
+    console.log(primeArray)
+  }
 
   function generateTable(event) {
     event.preventDefault()
     let inputNumber = parseInt(document.querySelector('#input-number').value)
-    console.log(inputNumber)
-    console.log(Number.isInteger(inputNumber))
+    generatePrimeArray(inputNumber)
   }
 })
