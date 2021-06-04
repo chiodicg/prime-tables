2dbd8d4b9ddd03104ac01e7ff0a05e0e69567767
Carla G. Chiodi <carla.chiodi@gmail.com>
2021-06-04 21:32:04 +0100

    Get the input integer
    Added function to get the input value submitted in the form and transformed it in integer

--- prime-tables.js
+++ prime-tables.js
@@ -1,1 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => {
+const tableForm = document.querySelector("#generate-table")
+tableForm.addEventListener("submit", generateTable)
+
+  function generateTable(event) {
+    event.preventDefault()
+    let inputNumber = parseInt(document.querySelector('#input-number').value)
+    console.log(inputNumber)
+    console.log(Number.isInteger(inputNumber))
+  }
+})
