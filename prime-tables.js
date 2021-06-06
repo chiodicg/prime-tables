b31c641e9b2bb9f279270f568004dc50d854f2d5
Carla G. Chiodi <carla.chiodi@gmail.com>
2021-06-06 20:37:05 +0100

    Added the multiplication
    Added the function to multiply the prime numbers and also wrote some comments

--- prime-tables.js
+++ prime-tables.js
@@ -2,7 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const tableForm = document.querySelector("#generate-table")
   tableForm.addEventListener("submit", generateTable)
   let primeArray = []
+  let multipliedPrimes = []
 
+    //Generating the prime multiplication table
+  function generateTable(event) {
+    event.preventDefault()
+    let inputNumber = parseInt(document.querySelector('#input-number').value)
+    generatePrimeArray(inputNumber)
+    multiplyPrime(primeArray)
+    displayTable(multipliedPrimes)
+  }
+
+  //Creating the array of N primes
+  function generatePrimeArray(n) {
+    let i = 0
+    primeArray = []
+    while (primeArray.length < n) {
+      if (isPrime(i)) {
+        primeArray.push(i)
+      } i++
+    }
+  }
+
+  //To check if the number is a prime number
   function isPrime(n) {
     if (n <= 1) {
       return false
@@ -19,20 +41,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  function generatePrimeArray(n) {
-    let i = 0
-    let primeArray = []
-    while (primeArray.length < n) {
-      if (isPrime(i)) {
-        primeArray.push(i)
-      } i++
-    }
-    console.log(primeArray)
+  //Multiplication of the N primes
+  function multiplyPrime() {
+    let firstRow = [null].concat(primeArray)
+    multipliedPrimes = [firstRow]
+    //For each prime number in the primeArray it will create a new row with this prime
+    primeArray.forEach(prime => {
+      let nextRow = [prime]
+      primeArray.forEach(multiplier => { //Multiply this prime for each item of the primeArray
+        nextRow.push(prime * multiplier)
+      })
+      multipliedPrimes.push(nextRow)
+    })
+    console.log(multipliedPrimes)
   }
 
-  function generateTable(event) {
-    event.preventDefault()
-    let inputNumber = parseInt(document.querySelector('#input-number').value)
-    generatePrimeArray(inputNumber)
+  //Display the multiplication table
+  function displayTable() {
   }
+
+
+
 })
